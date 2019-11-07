af34b06e65df2e3bb48270d07c821d1f536184d6
Julien Londeix <jlondeix@gmail.com>
2019-11-07 11:33:35 +0100

    Edited DynamoDB localization

--- libs/dynamodb-lib.js
+++ libs/dynamodb-lib.js
@@ -1,7 +1,7 @@
 import AWS from "aws-sdk";
 
 export function call(action, params) {
-    AWS.config.update({ region: "eu-west-3" });
+    AWS.config.update({ region: "eu-central-1" });
     const dynamoDb = new AWS.DynamoDB.DocumentClient();
     return dynamoDb[action](params).promise();
 }
